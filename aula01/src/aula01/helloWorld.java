ff9359486d2f256fc70f86378dee18b773d2c112
Gabriel Dias <87398682+gabriieldiis@users.noreply.github.com>
2023-03-02 22:53:32 -0300

    Aula 01
    Aula 01

---
+++ aula01/src/aula01/helloWorld.java
@@ -0,0 +1,36 @@
+package aula01;
+
+//esse é um comentário em Java
+//int number = 0; ajuda do luis
+//boa pratica Java
+
+//Camel Case AquiEstaUmExemploDeCamelCase
+//Snake Case aqui_esta_um_exemplo_de_snake_case
+
+public class helloWorld {
+	public static void main(String[] args) {
+		
+		
+		/*Como fazer um loop em java
+		int a = 0;
+		while (a < 100) {
+			System.out.print("aaaa");
+			a++;
+		}*/
+		
+		String nome = "Gabriel";
+		int idade = 22;
+		
+		System.out.println("Hello World!");
+		System.out.println("------------");
+		System.out.println("Ola " + nome);
+		System.out.println("------------");
+		System.out.println("Para concatenar em Java usamos o operador +");
+		System.out.println("------------");
+		System.out.println("Sempre lembre-se de usar boas praticas em Java");
+		System.out.println("- Luis Felipe Santana de Almeida");
+		System.out.println("-------------");
+		System.out.println("Minha idade e " + idade);
+		System.out.println("---------------");
+	}
+}
